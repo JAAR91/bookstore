47b21137fb1e2600060bcadce4e04464585798bf
JAAR91 <jaarriagaramos@gmail.com>
2021-10-05 14:48:15 -0600

    create redux store and reduce inside redux folder

--- src/components/BooksContainer.js
+++ src/components/BooksContainer.js
@@ -1,29 +1,15 @@
 import React from 'react';
 import Book from './Book';
 import FormContainer from './FormContainer';
+import store from '../redux/configureStores';
 
 const BooksContainer = () => {
-  const books = [{
-    id: 1,
-    title: 'Lord of the Rings',
-    author: 'Novel by J. R. R. Tolkien',
-    category: 'Fantasy',
-    progress: 'chapter 2',
-    complete: '10%',
-  },
-  {
-    id: 2,
-    title: 'Harry Potter',
-    author: 'J. K. Rowling',
-    category: 'Fantasy',
-    progress: 'chapter 4',
-    complete: '20%',
-  }];
-
+  const books = store.getState();
+  console.log(books);
   return (
     <>
       <div className="container px-0 py-4 border-bottom">
-        {books.map((book) => (
+        {books.booksReducer.map((book) => (
           <div key={book.id}>
             <Book bookdata={book} />
           </div>
---
+++ src/redux/books/books.js
@@ -0,0 +1,49 @@
+const ADD_BOOK = 'bookStore/books/ADD_BOOK';
+const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
+
+const initialState = [{
+  id: 1,
+  title: 'Lord of the Rings',
+  author: 'Novel by J. R. R. Tolkien',
+  category: 'Fantasy',
+  progress: 'chapter 2',
+  complete: '10%',
+},
+{
+  id: 2,
+  title: 'Harry Potter',
+  author: 'J. K. Rowling',
+  category: 'Fantasy',
+  progress: 'chapter 4',
+  complete: '20%',
+}];
+
+export const addBook = (payload) => ({
+  type: ADD_BOOK,
+  payload,
+});
+
+export const removeBook = (payload) => ({
+  type: REMOVE_BOOK,
+  payload,
+});
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_BOOK:
+      return [...state, {
+        id: state[state.length].id + 1,
+        title: action.payload.title,
+        author: 'default',
+        category: action.payload.category,
+        progress: 'chapter 1',
+        complete: '0%',
+      }];
+    case REMOVE_BOOK:
+      return state.filter((book) => book.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
+
+export default reducer;
